8559727e14cb043b27075eb7af91a5cbbd377c8d
Your Name <you@example.com>
2023-11-29 17:19:27 +0530

    add heading checkbox

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [gender, setGender] = useState("male");
   return (
     <div className="App">
+      <h3>Check Box</h3>
       <input
         type="checkbox"
         checked={gender === "male"}
